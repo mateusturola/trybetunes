d4ada78b0a4e5fdeef8a8b901bc2754360dda869
mateusturola <turolamateus@gmail.com>
2021-11-16 23:26:26 -0300

    Add css no carregando

--- src/Components/Load.jsx
+++ src/Components/Load.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import '../Styles/loading.css';
+
 
 class Load extends Component {
   render() {
     return (
-      <div>
+      <div className="load-box">
+        <div class="c-loader"></div>
         <h3 className="load-text">Carregando...</h3>
       </div>
     );
